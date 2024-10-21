4506edab5d215ef7a183bd995432c8b4b3b325d1
Marine Bardou <marine.bardou@uclouvain.be>
2024-10-21 16:25:15 +0200

    Enoncé exercice medium. Premier draft

--- exercises/medium_difficulty_exercise.R
+++ exercises/medium_difficulty_exercise.R
@@ -1,1 +1,36 @@
 ##### script medium exercise #####
+
+# Goal: 
+# You are working on the European Green Deal.
+# It is a new legislative term now in the EU. 
+# So you want to know what is the state of play for different legislation packages. 
+
+# 1. First, we are going to list all relevant procedures. 
+# In the EU, once proposed each piece of legislation has a procedure number, including 'COD'. 
+# Go to this page that lists the legislative files under the Green Deal. 
+# These procedure numbers are not immediately visible on the page. 
+
+# 2. Now you have listed the names of all relevant procedures. 
+# Go to this website, and search for one of them. 
+# Observe the results.Look at the URL when you click on the procedure's page. 
+# What can you see? 
+
+# 3. Construct the URLs for each of the procedure that we listed. 
+# For that purpose, use the function paste. 
+# Store the procedure numbers and the corresponding URLs in a single data frame. 
+
+# 4. Take a single URL link. 
+# In a separate data frame (which will have only one line), scrape
+# - the name of the rapporteur
+# - the date at which the legislative file was proposed
+# - the date at which the legislative file was adopted
+
+# 5. Write a function that automates the scraping you did at question 4. 
+# For each URL, the function has to scrape the same three information. 
+# Run that function and store the results in a data frame that also contains the number of the procedures and their URLs.
+
+# 6. Calculate the duration of each legislative process in a new column of your data frame. 
+
+
+
+
