31d645ae9d9430e389c7f19ed64852c3064cd761
Marine Bardou <marine.bardou@uclouvain.be>
2024-10-28 20:12:43 +0100

    Test bug

--- exercises/medium_difficulty_exercise.R
+++ exercises/medium_difficulty_exercise.R
@@ -1,6 +1,7 @@
 ##### script medium exercise #####
 
 # Goal: 
+# TEST !!!! 
 # You want to know what happened to the files which were EU legislative priorities in 2023-2024
 # So you want to know what is the state of play for different legislation packages. 
 
